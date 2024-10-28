a2095bd9f3555b3b65840a7cefffd551132a0beb
Pavlo Yahoda <pavlo.yahoda@gmail.com>
2024-10-28 09:30:19 +0200

    + additional task in task5.js

--- jsBasics/task5.js
+++ jsBasics/task5.js
@@ -5,20 +5,40 @@
 let users = [
     {name: "Jack Black", email: "jack.black@mail.co", age: 39},
     {name: "John Gaspar", email: "john.gaspar@mail.co", age: 53},
-    {name: "Hankey Bannister", email: "hankey.bannister@mail.co", age: 28}
+    {
+        name: "Hankey Bannister", 
+        email: "hankey.bannister@mail.co", 
+        age: 28, 
+        overridedToString: (users) =>  {
+            for (let user of users){
+                const {name, email, age} = user;
+                console.log(name);
+                console.log(email);
+                console.log(age);
+                console.log("***********");
+            }
+        }
+    }
 ];
 
-for (let user of users){
-    for (let key in user){
-        console.log(key + ': ' + user[key]);
-    }
-};
+// for (let user of users){
+//     for (let key in user){
+//         console.log(key + ': ' + user[key]);
+//     }
+// };
 
-//or
-console.log("*************************************");
-for (let user of users){
-    const {name, email, age} = user;
-    console.log(name);
-    console.log(email);
-    console.log(age);
+// //or
+// console.log("*************************************");
+// for (let user of users){
+//     const {name, email, age} = user;
+//     console.log(name);
+//     console.log(email);
+//     console.log(age);
+// }
+
+for (let user of users) {
+    if (typeof user.overridedToString === 'function') {
+        user.overridedToString(users);
+        break;
+    }
 }
