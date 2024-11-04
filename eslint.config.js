b9a810a3300889c00ff0c413583d616340882825
Pavlo Yahoda <pavlo.yahoda@gmail.com>
2024-11-04 20:25:13 +0200

    + rules

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,10 @@ import pluginReact from "eslint-plugin-react";
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
   {languageOptions: { globals: globals.browser }},
+  { "rules": {
+    "semi": ["error", "always"]
+    }
+  }
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
