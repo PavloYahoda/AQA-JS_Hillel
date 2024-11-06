3591533f36b8a0f7aff7e323176ff9e69581c170
Pavlo Yahoda <pavlo.yahoda@gmail.com>
2024-11-06 11:27:06 +0200

    homework12: +1

--- jsBasics/task1.js
+++ jsBasics/task1.js
@@ -1,4 +1,4 @@
 //ESLint + Prettier
 // На цьому фрагменті видно, що працює ESLint і можна форматувати Shift + Alt + F
-let           abc = "abc"
-            console.log(abc)
+let           abc = "abc";
+            console.log(abc);
